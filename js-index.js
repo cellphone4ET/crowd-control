c58d1e536d61a3878a16effbc47ba39019e49e01
Erica Johnson <ericacjohnson@gmail.com>
2018-04-24 10:25:59 -0700

    document ready function added to fix uncaught type error

--- js-index.js
+++ js-index.js
@@ -1,13 +1,73 @@
+$(document).ready(function() {
+"use strict";
+
 const state = {
-	userLocation: {
-		latitude: [],
-		longitude: []
-	},
-	userDates: {
-		beginDate: ['YYYY-MM-DD'],
-		endDate: ['YYYY-MM-DD']
+	userLocation: {},
+	userDates: {}
+};
+
+var map;
+var geocoder;
+
+function init() {
+	//console.log('init ran');
+	geocoder = new google.maps.Geocoder();
+	var latlng = new google.maps.LatLng(51.531703, -0.124310);
+	var mapOptions = {
+		zoom: 14,
+		center: latlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map($('.map'), mapOptions);
+
+}
+
+
+function getCurrentLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+
+			var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+			state.userLocation = pos;
+			getDataFromAPI(lat, lng);
+
+			map.setCenter(pos);
+		}, function() {
+			handleLocationError(true, infoWindow, map.getCenter());
+		});
+	} else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
 	}
 };
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+		'Error: The Geolocation service failed.' :
+		'Error: Your browser doesn\'t support geolocation.');
+}
+
+function geoCodeSearch(address) {
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+      	console.log(results[0].geometry.location);
+      	let lat = results[0].geometry.location.lat();
+      	let lng = results[0].geometry.location.lng();
+      	getDataFromAPI(lat, lng);
+        // map.setCenter(results[0].geometry.location);
+        // var marker = new google.maps.Marker({
+        //     map: map,
+        //     position: results[0].geometry.location
+        // });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+}
 
 function getDataFromAPI(lat, lng) {
 	let SEAT_GEEK_URL = "https://api.seatgeek.com/2/events";
@@ -32,41 +92,27 @@ function getDataFromAPI(lat, lng) {
 	$.ajax(settings);
 }
 
-function printData(data) {
-	return console.log('working');
-}
-
-function submitButton() {
+//event listeners
+function submitManualLocationInput() {
 	$('#main-submit-form').on('submit', function(event) {
 		event.preventDefault();
 		let address= $('#location-input').val();
 		geoCodeSearch(address);
+		$('#location-input').val(' ');
 	})
 }
 
-function geoCodeSearch(address) {
-	let geocoder = new google.maps.Geocoder();
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == 'OK') {
-      	console.log(results[0].geometry.location);
-      	let lat = results[0].geometry.location.lat();
-      	let lng = results[0].geometry.location.lng();
-      	getDataFromAPI(lat, lng);
-        // map.setCenter(results[0].geometry.location);
-        // var marker = new google.maps.Marker({
-        //     map: map,
-        //     position: results[0].geometry.location
-        // });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-  }
+function submitGeoLocation() {
+	$('#geolocation-button').on('click', function(event) {
+		init();
+		getCurrentLocation();
+	})
+}
 
- function init() {
- 	console.log('hi');
- }
+submitGeoLocation();
+submitManualLocationInput();
+
+});
 
 
 
-submitButton();
