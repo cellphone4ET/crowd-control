29847fbd0d6bad337472d4583b105b9325e604bd
Erica Johnson <ericacjohnson@gmail.com>
2018-04-30 11:44:58 -0700

    version for user review, no styling

--- js-index.js
+++ js-index.js
@@ -38,7 +38,7 @@ function getCurrentLocation() {
 		});
 	} else {
 		// Browser doesn't support Geolocation
-		handleLocationError(false, infoWindow, map.getCenter());
+		alert(`Geolocation wasn't successful, try searching for your location instead.`)
 	}
 }
 
@@ -49,10 +49,10 @@ function geoCodeSearch(address) {
 		if (status == 'OK') {
 			state.userLocation.lat = results[0].geometry.location.lat();
 			state.userLocation.lng = results[0].geometry.location.lng();
-			geocodeSuccess()
+			geocodeSuccess();
 		} else {
-			console.log(status)
-			alert(`Geocode wasn't successful, try searching manually`);
+			// Browser doesn't support Geolocation
+			handleLocationError(false, infoWindow, map.getCenter());
 		}
 	});
 }
@@ -68,87 +68,114 @@ function geocodeSuccess(){
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 	infoWindow.setPosition(pos);
 	infoWindow.setContent(browserHasGeolocation ?
-		'Error: The Geolocation service failed.' :
-		'Error: Your browser doesn\'t support geolocation.');
-	}
+	'Error: The Geolocation service failed.' :
+	'Error: Your browser doesn\'t support geolocation.');
+	alert('Whoops! Looks like geolocation is having some issues. Please\
+		search for location instead.');
+}
 
 
-	function getDataFromAPI(lat, lng) {
-		let SEAT_GEEK_URL = "https://api.seatgeek.com/2/events";
-		let settings = {
-			url: SEAT_GEEK_URL,
-			data: {
-				lat: lat,
-				lon: lng,
-				per_page: 25,
-				client_id: `MTEyMzQzMjd8MTUyMzgyODA5MS41NA`,
-				client_secret: 'ff1ee01bc647aabb41616ad3d3d3d340eb2ed31f38dd732953388a38285cccde'
-			},
-			dataType: 'json',
-			type: 'GET',
-			success: function(data) {
-				displayData(data);
-			},
-			error: function(error) {
-				console.log(error);
-			}
-		};
-		$.ajax(settings);
-	}
+function getDataFromAPI(lat, lng) {
+	let SEAT_GEEK_URL = "https://api.seatgeek.com/2/events";
+	let settings = {
+		url: SEAT_GEEK_URL,
+		data: {
+			lat: lat,
+			lon: lng,
+			per_page: 25,
+			client_id: `MTEyMzQzMjd8MTUyMzgyODA5MS41NA`,
+			client_secret: 'ff1ee01bc647aabb41616ad3d3d3d340eb2ed31f38dd732953388a38285cccde'
+		},
+		dataType: 'json',
+		type: 'GET',
+		success: function(data) {
+			displayData(data);
+		},
+		error: function(error) {
+			console.log(error);
+		}
+	};
+	$.ajax(settings);
+}
 
-	//functions to display data
-	function displayData(data) {
-		console.log(data);
-		let results = data.events;
-		let events = results.map(function(event) {
-			var markerPos = {
-				lat: event.venue.location.lat,
-				lng: event.venue.location.lon
-			}
+//functions to display data
+function displayData(data) {
+	console.log(data);
+	let results = data.events;
+	let events = results.map(function(event) {
+		var markerPos = {
+			lat: event.venue.location.lat,
+			lng: event.venue.location.lon
+		}
 
-			var contentString = `
-			<div>
-			<h1>event name</h1>
-			On ${event.datetime_local} there will be a large crowd around\
-			${event.venue.address}. May be best to avoid that area.
-			</div>`
+		var contentString = `
+		<div>
+		<h1>${event.short_title}</h1>
+		<h2>${event.venue.name}</h2>
+		On ${event.datetime_local} there will be a <br> large crowd around\
+		${event.venue.address}. <br><br>It may be best to avoid that area.
+		</div>`
 
-			createMarker(markerPos, contentString);
-		})
-	}
+		createMarker(markerPos, contentString);
+	})
+}
 
-	function createMarker(markerPos, contentString) {
-		var marker = new google.maps.Marker({
-			position: markerPos,
-			map: map,
-			title: "Introverts beware!",
-			infowindow: infoWindow,
-			//icon: 'images/icn_blue.png'
-		});
-		marker.addListener('click', function(){
-			infoWindow.open(map, marker);
-			infoWindow.setContent(contentString)
-		})
-		state.markers.push(marker)
-	}
-
-	//event listeners
-	function submitManualLocationInput() {
-		$('#main-submit-form').on('submit', function(event) {
-			event.preventDefault();
-			let address= $('#location-input').val();
-			geoCodeSearch(address);
-			$('#location-input').val(' ');
-		})
-	}
-
-	function submitGeoLocation() {
-		$('#geolocation-button').on('click', function(event) {
-			getCurrentLocation();
-		})
-	}
-
-	$(document).ready(function() {
-		submitGeoLocation();
-		submitManualLocationInput();
+function createMarker(markerPos, contentString) {
+	var marker = new google.maps.Marker({
+		position: markerPos,
+		map: map,
+		title: "Introverts beware!",
+		infowindow: infoWindow,
+		//icon: 'images/icn_blue.png'
 	});
+	marker.addListener('click', function(){
+		infoWindow.open(map, marker);
+		infoWindow.setContent(contentString)
+	})
+	state.markers.push(marker)
+}
+
+function hideShowElementsOnSubmit() {
+	$('#main-submit-form').hide();
+	$('#map').show();
+	$('#results-menu').show();
+}
+//event listeners
+function submitManualLocationInput() {
+	$('#main-submit-form').on('submit', function(event) {
+		event.preventDefault();
+		let address = $('#location-input').val();
+		geoCodeSearch(address);
+		$('#location-input').val(' ');
+		hideShowElementsOnSubmit();
+	})
+}
+
+function submitGeoLocation() {
+	$('#geolocation-button').on('click', function(event) {
+		getCurrentLocation();
+		hideShowElementsOnSubmit();
+
+	});
+}
+
+function submitFutureDate() {}
+
+function submitNewLocation(){
+	$('#new-location-form').on('submit', function(event) {
+		event.preventDefault();
+		init();
+		let address = $('#new-location-input').val();
+		console.log(address)
+		geoCodeSearch(address);
+		$('#new-location-input').val(' ');
+		hideShowElementsOnSubmit();
+	});
+}
+
+$(document).ready(function() {
+	// submitFutureDate();
+	submitNewLocation();
+	submitGeoLocation();
+	submitManualLocationInput();
+});
